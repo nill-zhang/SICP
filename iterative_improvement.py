17843953e1c3e74008395efcc3f639e7ed5dcaa6
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-17 17:02:57 -0400

    refactor and add comment

--- iterative_improvement.py
+++ iterative_improvement.py
@@ -1,10 +1,13 @@
 def improve(update, close, guess=1):
+    """a general function keeps updating a number
+       based on two func parameters """
     while not close(guess):
         guess = update(guess)
     return guess
 
 
 def approximate_equal(x, y, tolerance=1e-3):
+    """ true if the absolute difference between the two params less than the tolerance"""
     return abs(x-y) < tolerance
 
 
@@ -21,6 +24,10 @@ def golden_ratio_test():
 
 
 def sqrt(a):
+    # functions will extend its local environment
+    # to its parent environments when they are called
+    # the functions average, square_update, square_close can see
+    # a and each other, because these names are in its parent environment(sqrt)
     def average(x, y):
         return (x + y) / 2
 
@@ -39,7 +46,7 @@ def sqrt_test():
 
 def newton_update(f, df):
     def update(guess):
-        return guess - f(guess)/df(guess)  # the next more ideal point
+        return guess - f(guess)/df(guess)  # get the next point
     return update
 
 
@@ -49,27 +56,61 @@ def newton_close(f):
     return close
 
 
-def square(x):            # can be replaced by lambda
-    return x * x - 49
+def newton_square(num):
+
+    def f(x):        # function value at point x
+        return x * x - num
+
+    def df(x):       # derivative of function at point x
+        return 2 * x
+
+    return improve(newton_update(f, df), newton_close(f))
 
 
-def square_df(x):
-    return 2 * x
+def newton_cube(num):
 
+    def f(x):
+        return x * x * x - num
 
-def cube(x):
-    return x * x * x - 27
+    def df(x):
+        return 3 * x * x
 
-
-def cube_df(x):
-    return 3 * x * x
+    return improve(newton_update(f,  df), newton_close(f))
 
 
 def newton_test():
-    square_root_of49 = improve(newton_update(square, square_df), newton_close(square))
+    square_root_of49 = newton_square(49)
     print("square root of 49 is {}".format(square_root_of49))
-    cubic_root_of27 = improve(newton_update(cube, cube_df), newton_close(cube))
+    cubic_root_of27 = newton_cube(27)
     print("cubic root of 27 is {}".format(cubic_root_of27))
+
+# initially, I write the following code, its not elegant at all
+# although it has the sam functionality
+# I hard coded the numbers, these numbers can be used as closure like
+# I did in the above refactored code.
+
+
+# def square(x):            # can be replaced by lambda
+#     return x * x - 49
+#
+#
+# def square_df(x):
+#     return 2 * x
+#
+#
+# def cube(x):
+#     return x * x * x - 27
+#
+#
+# def cube_df(x):
+#     return 3 * x * x
+#
+#
+# def newton_test():
+#     square_root_of49 = improve(newton_update(square, square_df), newton_close(square))
+#     print("square root of 49 is {}".format(square_root_of49))
+#     cubic_root_of27 = improve(newton_update(cube, cube_df), newton_close(cube))
+#     print("cubic root of 27 is {}".format(cubic_root_of27))
 
 
 if __name__ == "__main__":
