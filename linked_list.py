cda3fe5a29c513e567ea76200e77412ab0520fef
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-29 02:42:56 -0400

    rewrite test suites

--- linked_list.py
+++ linked_list.py
@@ -5,7 +5,7 @@ def linked_list(val, nex):
 
 
 def is_empty(lst):
-    """ returns True if it is a linked list """
+    """ returns True if the linked list is empty """
     return lst == []
 
 
@@ -23,9 +23,9 @@ def next(lst):
 
 def join_linked_lists(lst1, lst2):
     """ joins the second linked list to the end of  the first one"""
-    assert is_linked_list(lst1) and is_linked_list(lst2),"not all operands are linked lists"
+    assert is_linked_list(lst1) and is_linked_list(lst2), "not all operands are linked lists"
     a = lst1
-    while not isEmpty(a):
+    while not is_empty(a):
         a = next(a)
     # a = a + lst2 there is indeed a difference between this line and the next
     a += lst2
@@ -44,7 +44,7 @@ def length(lst):
 
 def get(lst, idx):
     """ return the value at index idx"""
-    assert is_empty(lst), 'linked list is empty!'
+    assert not is_empty(lst), 'linked list is empty!'
     assert idx in range(length(lst)), 'linked list index out of range'
     # range(length(lst)) will be executed once and only once
     # although lst is rebound later to its next
@@ -59,9 +59,9 @@ def get(lst, idx):
 def set(lst, idx, val):
     """ resets the value at idx"""
     # the design behind this function is to think about
-    # how would you change a linked list inplace, or build and return
+    # how would you change a linked list in place, or build and return
     # new linked list
-    assert idx in range(length(lst)),"linked list index out of range"
+    assert idx in range(length(lst)), "linked list index out of range"
     for i in range(length(lst)):
         if idx == i:
             lst[0] = val
@@ -166,41 +166,108 @@ def is_linked_list(lst):
 
 
 def minimum_test():
-    lst = linked_list(-12, linked_list(33, linked_list(900, linked_list(-2))))
+    lst = linked_list(-12, linked_list(33, linked_list(900, linked_list(-2, []))))
     assert minimum(lst) == -12, "minimum function failed"
 
 
 def maximum_test():
-    lst = linked_list(-12, linked_list(33, linked_list(900, linked_list(-2))))
+    lst = linked_list(-12, linked_list(33, linked_list(900, linked_list(-2, []))))
     assert maximum(lst) == 900, "maximum function failed"
 
 
 def is_linked_list_test():
     lst = []
-    assert is_linked_list(lst),"is_linked_list failed"
-    lst = linked_list(3,[])
-    assert is_linked_list(lst),"is_linked_list failed"
+    assert is_linked_list(lst), "is_linked_list failed"
+    lst = linked_list(3, [])
+    assert is_linked_list(lst), "is_linked_list failed"
 
 
-def linked_list_test():
-    list_a = linked_list(13, linked_list(9, linked_list(42, linked_list(5, linked_list(3, [])))))
-    list_b = linked_list(93, linked_list(900, linked_list(22, [])))
-    list_c = linked_list_from_list([4.0, 2.1, 0.12, 5.6, 7.7, 2.5])
-    print("lnklst a: ", list_a)
-    print("lnklst b: ", list_b)
-    print("lnklst c: ", list_c)
-    join_linked_lists(list_a, [-0.11, [-0.111, []]])
-    print("lnklst a: ", list_a)
-    list_a = keep_if(lambda x: x > 0, list_a)
-    print("positive elements in lnklst a: ", list_a)
-    list_b = apply_to_all(lambda x: x + 1000, list_b)
-    print("add 1000 to every elements in lnklst b:", list_b)
-    list_d = flatten(list_c)
-    print("flattened list_c:", list_d)
-    list_e = reverse(list_a)
-    print("list_a reversed: ", list_e)
-    print("is [] a linked list ? ", is_linked_list([]))
+def length_test():
+    a = []
+    assert length(a) == 0, "length_test failed"
+    b = [4, a]
+    assert length(b) == 1, "lengt)_test failed"
+
+
+def set_test():
+    a = [1, [5, [19, [111, []]]]]
+    set(a, 2, 9999)
+    assert a == [1, [5, [9999, [111, []]]]], "set_test failed"
+
+
+def get_test():
+    a = [4, [1, [3.4, []]]]
+    b = get(a, 2)
+    assert b == 3.4, "get_test failed"
+
+
+def index_test():
+    a = []
+    assert index(a, 11) is None, "index_test failed"
+
+    b = [1, [2, [3, [90, [888, [131, []]]]]]]
+    assert index(b, 888) == 4, "index_test failed"
+
+
+def reverse_test():
+    a = [5, [4, [3, [2, [1, []]]]]]
+    b = reverse(a)
+    assert b == [1, [2, [3, [4, [5, []]]]]]
+
+
+def min_test():
+    a = [0.1, [0.44, [0.1122, [0.42112, [0.428, [-0.1232, [-2, [31, [-1,[]]]]]]]]]]
+    assert minimum(a) == -2, "minimum_test failed"
+
+
+def max_test():
+    a = [0.1, [0.44, [0.1122, [0.42112, [0.428, [-0.1232, [-2, [31, [-1, []]]]]]]]]]
+    assert maximum(a) == 31, "maximum_test failed"
+
+
+def flatten_test():
+    a = []
+    assert flatten(a) == [], "flatten_test failed"
+    b = [1, [3, [0.22, [-22, []]]]]
+    assert flatten(b) == [1, 3, 0.22, -22]
+
+
+def linked_list_from_list_test():
+    a = linked_list_from_list([])
+    assert is_linked_list(a), "linked_list_from_list failed"
+    b = linked_list_from_list(['a', 'c', 'd'])
+    assert is_linked_list(b), "linked_list_from_list failed"
+
+
+def apply_to_all_test():
+    a = linked_list_from_list([1, 2, 3, 4, 5, 6])
+    b = apply_to_all(lambda x: x**2, a)
+    assert b == [1, [4, [9, [16, [25, [36, []]]]]]], "apply_to_all_test failed"
+
+
+def keep_if_test():
+    a = linked_list_from_list([3, 4, 12, 13, 0.1])
+    b = keep_if(lambda x: x**2 >= 144, a)
+    assert b == [12, [13, []]], "keep_if_test failed"
+
+
+def join_linked_lists_test():
+    a = linked_list_from_list([1, 2])
+    b = linked_list_from_list(['a', 'b'])
+    assert join_linked_lists(a, b) == [1, [2, ['a', ['b', []]]]]
 
 
 if __name__ == "__main__":
-    linked_list_test()
+    join_linked_lists_test()
+    minimum_test()
+    maximum_test()
+    set_test()
+    get_test()
+    is_linked_list_test()
+    linked_list_from_list_test()
+    keep_if_test()
+    apply_to_all_test()
+    index_test()
+    reverse_test()
+    flatten_test()
+    length_test()
